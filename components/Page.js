dd2ec1ccf680059ba948649ec0a707e6c34be94b
cjmaret <cjmaret@gmail.com>
2022-09-24 19:01:20 -0400

    fix: replace fjalla one

--- components/Page.js
+++ components/Page.js
@@ -48,7 +48,7 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family:  --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'Fjalla One', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     padding: 0;
     margin: 0;
     font-size: 1.5rem;
